759e778fde0f88c02082db08040478e19197adf6
toChaim <toChaim@gmail.com>
2017-11-03 23:06:28 -0700

    add time to activity.

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ class App extends Component {
     this.state = {
       time: new Date(),
       activities: localStorage.getItem('activities') || {
-        work: { _id: 0, order: 0, title: 'Work' },
-        rest: { _id: 1, order: 1, title: 'Rest' }
+        work: { _id: 0, order: 0, title: 'Work', time: '00:25:00' },
+        rest: { _id: 1, order: 1, title: 'Rest', time: '00:05:00' }
       },
       activity: localStorage.getItem('activity') || 0
     };
--- src/molecules/Activity.js
+++ src/molecules/Activity.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
+import Time from '../atoms/Time';
 
 const ActivityStyle = styled.div`
   font-family: 'Anonymous Pro', monospace;
   border: 1px solid grey;
 `;
 
-const Activity = ({ activity }) => (
-  <ActivityStyle>{activity.title}</ActivityStyle>
-);
+const Activity = ({ activity }) => {
+  var strTime = new Date().toISOString().slice(0, 11) + activity.time + '.000Z';
+  return (
+    <ActivityStyle>
+      {activity.title} <Time time={new Date(strTime)} />
+    </ActivityStyle>
+  );
+};
 
 export default Activity;

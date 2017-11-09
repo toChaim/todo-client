0c5c172068c248228513d789785e276f37abaa3f
toChaim <toChaim@gmail.com>
2017-11-09 15:42:42 -0800

    start styling activities.

--- src/molecules/Activity.js
+++ src/molecules/Activity.js
@@ -5,6 +5,8 @@ import Time from '../atoms/Time';
 const ActivityStyle = styled.div`
   font-family: 'Anonymous Pro', monospace;
   border: 1px solid grey;
+  display: flex;
+  justify-content: space-around;
 `;
 
 const Activity = ({ activity }) => (
